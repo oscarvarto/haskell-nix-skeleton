2e45b06a62e31d387241b76f82b1cb99a0cd342e
Oscar Vargas Torres <oscarvargas@s4n.co>
2019-08-30 11:35:24 -0500

    Asking for help

--- nix/nix-ghc-ptr.nix
+++ nix/nix-ghc-ptr.nix
@@ -1,4 +1,8 @@
 {
   hieVer = "ghc864";
-  pinnedPkgs = import ./pkgs-from-json.nix { json = ./pkgs/nix-ghc864.json; };
+  # pinnedPkgs = import ./pkgs-from-json.nix { json = ./pkgs/nix-ghc864.json; };
+  pinnedPkgs = import (fetchTarball { 
+      url = "https://github.com/NixOS/nixpkgs/tarball/master";
+      #sha256 = "..."; 
+    }) {};
 }
--- shell.nix
+++ shell.nix
@@ -1,19 +1,30 @@
-let 
+let
   ghcPtr = import ./nix/nix-ghc-ptr.nix;
   pinnedPkgs = ghcPtr.pinnedPkgs;
   myPackages = (import ./release.nix { withHoogle = true; } );
   all-hies = import (fetchTarball "https://github.com/infinisil/all-hies/tarball/master") {};
 
   projectDrvEnv = myPackages.project1.env.overrideAttrs (oldAttrs: rec {
-    buildInputs = oldAttrs.buildInputs ++ [ 
+    buildInputs = oldAttrs.buildInputs ++ [
       pinnedPkgs.haskellPackages.cabal-install
       pinnedPkgs.haskellPackages.hlint
       all-hies.versions.${ghcPtr.hieVer}
-      ];
+      (pinnedPkgs.vscode-with-extensions.override {
+        vscodeExtensions = with pinnedPkgs.vscode-extensions; [
+          alanz.vscode-hie-server
+        ]
+        ++ pinnedPkgs.vscode-utils.extensionsFromVscodeMarketplace [{
+            name = "language-haskell";
+            publisher = "justusadam";
+            version = "2.6.0";
+            sha256 = "1891pg4x5qkh151pylvn93c4plqw6vgasa4g40jbma5xzq8pygr4";
+        }];
+      })
+    ];
     shellHook = ''
-      export USERNAME="christian.henry"
+      export USERNAME="oscarvarto"
       export HIE_HOOGLE_DATABASE="$NIX_GHC_LIBDIR/../../share/doc/hoogle/index.html"
     '';
   });
-in 
-  projectDrvEnv
+in
+projectDrvEnv
